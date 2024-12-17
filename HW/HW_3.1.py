3484bd7e39e5bc99a318f042dd5d8f27c2665810
DIMA <dima221crash@gmail.com>
2024-12-17 23:51:57 +0200

    HW3.1

---
+++ HW/HW_3.1.py
@@ -0,0 +1,23 @@
+number1 = int(input('Enter the first number:'))
+number2 = int(input('Enter the second number:'))
+user_select = int(input("1. +\n2. -\n3. *\n4. /\nSelect your choice: "))
+
+match user_select:
+    case 1:
+        result = number1 + number2
+        print(result)
+    case 2:
+        result = number1 - number2
+        print(result)
+    case 3:
+        result = number1 * number2
+        print(result)
+    case 4:
+        match number1 == 0 or number2 == 0:
+            case 1:
+                print('Invalid input please try again')
+            case _:
+                result = number1 / number2
+                print(result)
+    case _:
+        print("Invalid input please try again")
